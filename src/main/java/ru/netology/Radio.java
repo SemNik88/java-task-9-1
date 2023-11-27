4a6df00008384d324f0771116a83a1c2d74feb10
SemNik <142649558+SemNik88@users.noreply.github.com>
2023-11-27 19:29:42 +0300

    Update Radio.java

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,57 +1,64 @@
-    package ru.netology;
-    public class Radio {
-        public  int currentVolume;
-        public  int currentStation;
-        public int getCurrentVolume() {
-            return currentVolume;
-        }
-        public int getCurrentStation() {
-            return currentStation;
-        }
-        public void setCurrentVolume(int newCurrentVolume) {
-            if (newCurrentVolume > 100) {
-                return;
-            }
-            if (newCurrentVolume < 0) {
-                return;
-            }
-            currentVolume = newCurrentVolume;
-        }
-        public void setCurrentStation(int newCurrentStation) {
-            if (newCurrentStation > 9) {
-                return;
-            }
-            if (newCurrentStation < 0) {
-                return;
-            }
-            currentStation = newCurrentStation;
-        }
+package ru.netology;
 
-        public void increaseVolume() {
-            if (currentVolume < 100) {
-                currentVolume = currentVolume + 1;
-            }
-        }
-        public void decreaseVolume() {
-            if (currentVolume > 0) {
-                currentVolume = currentVolume - 1;
-            }
-        }
-        public void next() {
-            if (currentStation < 9) {
-                currentStation = currentStation + 1;
-            } else {
-                currentStation = 0;
-            }
-        }
+public class Radio {
+    private int currentVolume; // поле сделано приватным
+    private int currentStation; // поле сделано приватным
 
-        public void prev() {
-            if (currentStation > 0) {
-                currentStation = currentStation - 1;
-            } else {
-                currentStation = 9;
-            }
-        }
-
-
+    public int getCurrentVolume() {
+        return currentVolume;
     }
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentVolume(int newCurrentVolume) {
+        if (newCurrentVolume > 100) {
+            return;
+        }
+        if (newCurrentVolume < 0) {
+            return;
+        }
+        currentVolume = newCurrentVolume;
+    }
+
+    public void setCurrentStation(int newCurrentStation) {
+        if (newCurrentStation > 9) {
+            return;
+        }
+        if (newCurrentStation < 0) {
+            return;
+        }
+        currentStation = newCurrentStation;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < 100) {
+            currentVolume = currentVolume + 1;
+        }
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        }
+    }
+
+    public void next() {
+        if (currentStation < 9) {
+            currentStation = currentStation + 1;
+        } else {
+            currentStation = 0;
+        }
+    }
+
+    public void prev() {
+        if (currentStation > 0) {
+            currentStation = currentStation - 1;
+        } else {
+            currentStation = 9;
+        }
+    }
+
+
+}
